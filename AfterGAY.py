6a56b79db761009a708ddb3f6d8f369a9b05f6d5
Chisura <123957216+Chisura@users.noreply.github.com>
2023-01-30 17:17:29 +0200

    Update AfterGAY.py

--- AfterGAY.py
+++ AfterGAY.py
@@ -133,7 +133,7 @@ async def on_message(message: disnake.Message) -> None:
     if 'мяу' in message.content.lower().split():
         await message.channel.send('КТО МЯУКАЕТ???')
     if 'спокойной ночи' in message.content.lower():
-        await message.channel.send('Споки! Мяу.')
+        await message.channel.send('Споки! мяу')
 
 
     await bot.process_commands(message)
