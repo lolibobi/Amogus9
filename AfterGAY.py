e0d8f878a0a303ef98dfe3717a8ae71ea9e8b977
Chisura <123957216+Chisura@users.noreply.github.com>
2023-01-30 23:45:53 +0200

    Update AfterGAY.py

--- AfterGAY.py
+++ AfterGAY.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
 async def on_message(message: disnake.Message) -> None:
     heydi_id = 482568996399349770
     heydi = disnake.utils.find(
-        lambda m: m.id == heydi_id, message.content.lower)
+        lambda m: m.id == heydi_id, message.guild.members)
     """
     без split лфуауафтерслейв - БОТ НЕ ОТВЕЧАЕТ фуау афтерслейв - ОТВЕЧАЕТ
     без lower = Автерслейв - БОТ НЕ ОТВЕЧАЕТ автерслейв - ОТВЕЧАЕТ
