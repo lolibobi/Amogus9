efb6aa538851fbbffcd99cd36287ed22f5ce0f29
Chisura <123957216+Chisura@users.noreply.github.com>
2023-02-01 01:23:35 +0200

    Update AfterGAY.py

--- AfterGAY.py
+++ AfterGAY.py
@@ -117,6 +117,9 @@ if __name__ == "__main__":
 
 @bot.event
 async def on_message(message: disnake.Message) -> None:
+    heydi = 482568996399349770
+    heydi = disnake.utils.find(
+            lambda m: m.id == heydi_id, message.content)
     """
     без split лфуауафтерслейв - БОТ НЕ ОТВЕЧАЕТ фуау афтерслейв - ОТВЕЧАЕТ
     без lower = Автерслейв - БОТ НЕ ОТВЕЧАЕТ автерслейв - ОТВЕЧАЕТ
@@ -137,7 +140,6 @@ async def on_message(message: disnake.Message) -> None:
         await message.channel.send('КТО МЯУКАЕТ???')
     if 'спокойной ночи' in message.content.lower():
         await message.channel.send('Споки! мяу')
-    heydi_id = 482568996399349770
     if heydi.mention in message.content:
         await message.channel.send('А КАКА ТЫ ЭТА СДЕЛОЛА?Э')
 
