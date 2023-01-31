e2c8c5e5808bf1f1883b054a3e1712cb8efa6958
Chisura <123957216+Chisura@users.noreply.github.com>
2023-02-01 01:53:08 +0200

    Update AfterGAY.py

--- AfterGAY.py
+++ AfterGAY.py
@@ -124,8 +124,8 @@ async def on_message(message: disnake.Message) -> None:
     """
 
 
-    if message.author == bot.user or message.author.bot:
-        return
+#    if message.author == bot.user or message.author.bot:
+#        return
     
     if 'хуйня' in message.content.lower().split():
         await message.channel.send('Согласен.')
@@ -136,7 +136,7 @@ async def on_message(message: disnake.Message) -> None:
     if 'мяу' in message.content.lower().split():
         await message.channel.send('КТО МЯУКАЕТ???')
     if 'спокойной ночи' in message.content.lower():
-        await message.channel.send('Споки! :emoji_24:')
+        await message.channel.send('Споки! мяу')
 
     await bot.process_commands(message)
    
