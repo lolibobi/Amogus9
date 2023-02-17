3656085930f3bde9cd9f558140fba71a2d06de1a
Chisura <123957216+Chisura@users.noreply.github.com>
2023-02-17 23:56:20 +0300

    Update alisa-normal.py

--- cogs/normal/alisa-normal.py
+++ cogs/normal/alisa-normal.py
@@ -1,1 +1,11 @@
+import random
 
+import aiohttp
+import disnake
+from disnake.ext import commands
+from disnake.ext.commands import Context
+
+from helpers import checks
+
+def setup(bot):
+    bot.add_cog(alisa(bot))
