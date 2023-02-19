79c4330c113762c76564a985efa7da019825289d
Chisura <123957216+Chisura@users.noreply.github.com>
2023-02-19 03:13:36 +0200

    Update test-normal.py

--- cogs/normal/test-normal.py
+++ cogs/normal/test-normal.py
@@ -58,7 +58,7 @@ class Test(commands.Cog, name="test-normal"):
         buttons = Choice()
         author = context.guild.members
         embed = disnake.Embed(
-                description="**Вапросик**\n" vopros,
+                description="**Вапросик**"\n vopros,
                 color=0x9C84EF
         )
         message = await context.send(embed=embed, view=buttons)
