674f03ddc92526e80db33cd4a54ddcd1431e9cd5
Chisura <123957216+Chisura@users.noreply.github.com>
2023-02-19 13:19:43 +0200

    Update test-normal.py

--- cogs/normal/test-normal.py
+++ cogs/normal/test-normal.py
@@ -66,13 +66,13 @@ class Test(commands.Cog, name="test-normal"):
         result1 = "Ответ верный. Маладец. Держи cockфетку."
         result2 = "Неверный ответ!!! Заебошу тяяяя!"
          
-        if vopros == vopros1 and buttons.choice == "Да":
+        if vopros == vopros1 and buttons.choice == button.confirm:
             embed = disnake.Embed(
                 title="Вапросик",
                 description= result1,
                 color=0x5FFC00
             )
-        if vopros == vopros2 and buttons.choice == "Нет":
+        if vopros == vopros2 and buttons.choice == button.cancel:
             embed = disnake.Embed(
                 title="Вапросик",
                 description=result1,
